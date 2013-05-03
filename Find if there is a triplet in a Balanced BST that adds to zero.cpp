99e2f8cb6a505d5ddf1df391de4c2ec7d3b2c5c8
icode123 <yantian_123@126.com>
2013-05-03 11:07:21 +0800

    Create Find if there is a triplet in a Balanced BST that adds to zero.cpp

---
+++ Find if there is a triplet in a Balanced BST that adds to zero.cpp
@@ -0,0 +1,7 @@
+// http://www.geeksforgeeks.org/find-if-there-is-a-triplet-in-bst-that-adds-to-0/
+//方法一：暴力求解法：时间复杂度O(n^3)；
+//方法二：辅助数组+有序数组3sum：时间复杂度O(n^2)，空间复杂度O(n)；
+//方法三：BST转化为DLL：时间复杂度O(n^2)，空间复杂度O(Logn)；
+//方法四：两个中序遍历+一个逆序中序遍历，时间复杂度O(n^2)，空间复杂度O(Logn)；
+
+
