abb2435367451660a31a5983c428032cb0f44751
icode123 <yantian_123@126.com>
2013-05-03 20:52:16 +0800

    Create Find k-th smallest element in BST (Order Statistics in BST).cpp

---
+++ Find k-th smallest element in BST (Order Statistics in BST).cpp
@@ -0,0 +1,59 @@
+//http://www.geeksforgeeks.org/find-k-th-smallest-element-in-bst-order-statistics-in-bst/
+
+//寻找BST中第K小元素
+//方法一：中序遍历，时间复杂度O(n)；
+#include <stack>
+
+struct TreeNode 
+{
+  int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/* Recursive helper function -- given a node, and an array containing
+the path from the root node up to but not including this node,
+print out all the root-leaf paths. */
+TreeNode *k_smallest_element_inorder(TreeNode* root,int k) 
+{
+	if (!root || k<=0)
+	{
+		return NULL;
+	}
+	stack<TreeNode*> s;
+	while (root)
+	{
+		s.push(root);
+		root=root->left;
+	}
+	TreeNode *node=NULL;
+	while (!s.empty())
+	{
+		node=s.top();
+		s.pop();
+		if (!(--k))
+		{
+			break;
+		}
+		if (node->right)
+		{
+			node=node->right;
+			while (node)
+			{
+				s.push(node);
+				node=node->left;
+			}
+		}
+	}
+	if (k)
+	{
+		return NULL;
+	}
+	return node;
+}
+
+//方法二：
+
+
+
