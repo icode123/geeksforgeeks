91f874fa745b0722a1eb68934652cdfe61df3bfd
icode123 <yantian_123@126.com>
2013-05-05 20:13:08 +0800

    Update Calculate Size of a tree.cpp

--- Calculate Size of a tree.cpp
+++ Calculate Size of a tree.cpp
@@ -1,2 +1,17 @@
 //http://www.geeksforgeeks.org/write-a-c-program-to-calculate-size-of-a-tree/
+#include <stack>
+struct TreeNode 
+{
+  int val;
+	TreeNode *left;
+	TreeNode *right;
+};
 
+int size(TreeNode *root)
+{
+	if (!root)
+	{
+		return 0;
+	}
+	else return size(root->left)+1+size(root->right);
+}
