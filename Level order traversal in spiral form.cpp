864584200984d2365ff9f467e3a0b8f9a4ea0e32
icode123 <yantian_123@126.com>
2013-05-06 14:13:49 +0800

    Update Level order traversal in spiral form.cpp

--- Level order traversal in spiral form.cpp
+++ Level order traversal in spiral form.cpp
@@ -1,2 +1,53 @@
 //http://www.geeksforgeeks.org/level-order-traversal-in-spiral-form/
 
+struct TreeNode 
+{
+  int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int v):val(v),left(NULL),right(NULL){ }
+};
+
+//递归，时间复杂度：最坏情况下 O(n^2)（退化树）；
+void printGivenLevel(TreeNode* root, int level, bool ltr)
+{
+	if (!root)
+	{
+		return;
+	}
+	if (level==1)
+	{
+		cout<<root->val<<" ";
+	}
+	else if (level>1)
+	{
+		if (ltr)
+		{
+			printGivenLevel(root->left,level-1,ltr);
+			printGivenLevel(root->right,level-1,ltr);
+		}
+		else
+		{
+			printGivenLevel(root->right,level-1,ltr);
+			printGivenLevel(root->left,level-1,ltr);
+		}
+	}
+}
+int height(TreeNode* node)
+{
+	if (!node)
+	{
+		return 0;
+	}
+	else return	 1+max(height(node->left),height(node->right));
+}
+void reverseLevelOrder(TreeNode* root)
+{
+	int h=height(root);
+	bool ltr=false;
+	for (int i=1;i<=h;i++)
+	{
+		printGivenLevel(root,i,ltr);
+		ltr=~ltr;
+	}
+}
