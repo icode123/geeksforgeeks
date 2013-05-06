59435d748f30aabd51729b6b25e468de7e16c33c
icode123 <yantian_123@126.com>
2013-05-06 22:29:23 +0800

    Update Reverse a Linked List in groups of given size.cpp

--- Linked-List/Reverse a Linked List in groups of given size.cpp
+++ Linked-List/Reverse a Linked List in groups of given size.cpp
@@ -1,2 +1,9 @@
 //http://www.geeksforgeeks.org/reverse-a-list-in-groups-of-given-size/
+/*
+Example:
+Inputs:  1->2->3->4->5->6->7->8->NULL and k = 3 
+Output:  3->2->1->6->5->4->8->7->NULL. 
 
+Inputs:   1->2->3->4->5->6->7->80->NULL and k = 5
+Output:  5->4->3->2->1->8->7->6->NULL. 
+*/
