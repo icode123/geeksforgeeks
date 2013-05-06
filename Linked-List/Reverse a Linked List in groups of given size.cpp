0908ca8070a8a2837127d1ad2c133c9814d0332c
icode123 <yantian_123@126.com>
2013-05-06 22:48:53 +0800

    Update Reverse a Linked List in groups of given size.cpp

--- Linked-List/Reverse a Linked List in groups of given size.cpp
+++ Linked-List/Reverse a Linked List in groups of given size.cpp
@@ -8,3 +8,29 @@ Inputs:   1->2->3->4->5->6->7->80->NULL and k = 5
 Output:  5->4->3->2->1->8->7->6->NULL. 
 */
 //时间复杂度：O(n) 
+struct Node
+{
+  int data;
+	Node *next;
+};
+
+Node *reverse (Node *head, int k)
+{
+	Node *cur=head;
+	Node *pre=NULL;
+	Node *next;
+    int count=0;
+	while (cur && count<k)
+	{
+		next=cur->next;
+		cur->next=pre;
+		pre=cur;
+		cur=next;
+		count++;
+	}
+	if (next)
+	{
+		head->next=reverse(next,k);
+	}
+	return pre;
+}
