5a166ced32ce78efcf224a933d3cef91c26fe215
icode123 <yantian_123@126.com>
2013-05-06 22:26:57 +0800

    Update QuickSort on Linked List.cpp

--- Linked-List/QuickSort on Linked List.cpp
+++ Linked-List/QuickSort on Linked List.cpp
@@ -65,3 +65,50 @@ struct Node
 	int data;
 	Node *next;
 };
+void swap ( int* a, int* b )
+{   int t = *a;      *a = *b;       *b = t;   }
+Node *lastNode(Node *root)
+{
+	while (root && root->next)
+		root = root->next;
+	return root;
+}
+//prev为l的前驱,tmp为分割点的前驱；
+Node* partition(Node *l, Node *h,Node *prev,Node * &tmp)
+{
+	int x  = h->data;
+	Node *i =prev;
+	for (Node *j = l; j != h; j = j->next)
+	{
+		if (j->data <= x)
+		{
+			i = (i == NULL)? l : i->next;
+			if (i!=j)
+			{
+				swap(&(i->data),&(j->data));
+			}
+		}
+	}
+	tmp=i;
+	i = (i == NULL)? l : i->next; 
+	if (i!=h)
+	{
+		swap(&(i->data),&(h->data));
+	}
+	return i;
+}
+void _quickSort(Node* l, Node *h, Node *prev)
+{
+	if (h && l && l!=h && l!=h->next && h!=prev)
+	{
+		Node *tmp=NULL;
+		Node *p = partition(l,h,prev,tmp);
+		_quickSort(l,tmp,prev);
+		_quickSort(p->next,h,p);
+	}
+}
+void quickSort(Node *head)
+{
+	Node *h = lastNode(head);
+	_quickSort(head,h,NULL);
+}
